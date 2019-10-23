2b1eb085313b45a22df116889a343c3c9a17f24a
papillon078 <ldc.morin@gmail.com>
2019-10-23 14:38:07 +0200

    exo1 corrigé

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
 function replaceImage(){
-let oldImage = document.getElementById("image1");
-oldImage.setAttribute("src", "assets/img/image1_2.jpg");
+document.getElementsById('image1').src='assets/img/image1_2.jpg'
 }
